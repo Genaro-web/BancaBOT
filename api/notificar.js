534975d9d54bc4e46e2f1d59de324c7b21d98621
Genaro-web <deivistrader58@gmail.com>
2025-10-27 11:55:16 -0400

    eliminadas cuentas

--- api/notificar.js
+++ api/notificar.js
@@ -25,13 +25,13 @@ const MAPA_CUENTAS = {
   "37589": "Norelis64", // tg katiuska
   "98059": "ANYER", 
   "77327": "DENIREX", 
-  "64783": "LEANDRY", // bot guipo
+ // "64783": "LEANDRY", // bot guipo
   "20510": "YINELY", // bot guipo
-  "76360": "FABIOLA", // bot GUIPO
+  // "76360": "FABIOLA", // bot GUIPO
   "37568": "ARIADNA", // bot Deivisp
   "32213": "DAIBELIS", // tg @manolo521
-  "39205": "MARIA", // bot Gerardo
-  "39213": "ATILIO", // bot gerardo
+ // "39205": "MARIA", // bot Gerardo
+ // "39213": "ATILIO", // bot gerardo
   "69454": "JOSE" // bot deivip
 };
 // ---------------------------------
